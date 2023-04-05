d7965835c00e44fed1d14778d37caaaf89beceb0
felipemarinhodev <felipmarinho@gmail.com>
2023-04-05 07:15:26 -0700

    chore: :construction: config ts-jest

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,9 @@
-// Imports
-
-// To Test
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Renders main page correctly', () => {
+test('Renders main page correctly', async () => {
+  render(<App />);
+  const buttonCount = await screen.findByRole('button');
+  expect(buttonCount.innerHTML).toBe('count is 0');
   expect(true).toBeTruthy();
 });
