f1afafd904cbd291736d6abf0bd6219832d74d0d
felipemarinhodev <felipmarinho@gmail.com>
2023-04-05 08:04:13 -0700

    test: :white_check_mark: ensure text appears when the count is more than one

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -9,9 +9,12 @@ test('Renders main page correctly', async () => {
   // Setup
   render(<App />);
   const buttonCount = await screen.findByRole('button');
+  const codeCount = await screen.queryByText(/The count is now:/);
 
   // Pre Expecations
   expect(buttonCount.innerHTML).toBe('count is 0');
+  expect(codeCount).not.toBeInTheDocument();
+  expect(await screen.queryByText(/The count is now:/)).not.toBeInTheDocument();
 
   // Init
   fireEvent.click(buttonCount)
@@ -19,4 +22,5 @@ test('Renders main page correctly', async () => {
 
   // Post Expectations
   expect(buttonCount.innerHTML).toBe('count is 2');
+  expect(await screen.queryByText(/The count is now:/)).toBeInTheDocument();
 });
