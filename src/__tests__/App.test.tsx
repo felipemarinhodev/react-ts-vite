f597ab1be6bef06b74cf5cd45ac42a0c3d9454c8
felipemarinhodev <felipmarinho@gmail.com>
2023-04-05 07:50:21 -0700

    test: :hammer: ensure the count change happens when clicking the button

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react';
+// Imports
+import { fireEvent, render, screen } from '@testing-library/react';
+
+// To Test
 import App from '../App';
 
+// Tests
 test('Renders main page correctly', async () => {
+  // Setup
   render(<App />);
   const buttonCount = await screen.findByRole('button');
+
+  // Pre Expecations
   expect(buttonCount.innerHTML).toBe('count is 0');
-  expect(true).toBeTruthy();
+
+  // Init
+  fireEvent.click(buttonCount)
+  fireEvent.click(buttonCount)
+
+  // Post Expectations
+  expect(buttonCount.innerHTML).toBe('count is 2');
 });
